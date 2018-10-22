2cb605fbea648b4854ce3da4541e50e71aacb3ef
mevanstron <michael@michaelgregoryevans.com>
2018-10-22 17:36:28 -0500

    changed structure of test.js to better fit adding in more tests

--- test/test.js
+++ test/test.js
@@ -3,18 +3,24 @@ const SuperFizzBuzz = require('../classes/SuperFizzBuzz');
 
 
 describe('SuperFizzBuzz Class', () => {
-  let fizzBuzz = new SuperFizzBuzz;
+  let fizzBuzz;
 
-  it('When no default min is provided, SuperFizzBuzz is initialized with a min of 1', () => {
-    assert.equal(fizzBuzz.min, 1);
+  describe('Default Initialization', () => {
+    fizzBuzz = new SuperFizzBuzz;
+
+    it('When no default min is provided, SuperFizzBuzz is initialized with a min of 1', () => {
+      assert.equal(fizzBuzz.min, 1);
+    });
+    it('When no default max is provided, SuperFizzBuzz is initialized with a max of 100', () => {
+      assert.equal(fizzBuzz.max, 100);
+    });
+    it('When no default wordNums is provided, SuperFizzBuzz is initialized with wordNums of [{word: "Fizz", num: 3}, {word: "Buzz", num:5}]', () => {
+      assert.equal(fizzBuzz.wordNums[0].word, "Fizz");
+      assert.equal(fizzBuzz.wordNums[0].num, 3);
+      assert.equal(fizzBuzz.wordNums[1].word, "Buzz");
+      assert.equal(fizzBuzz.wordNums[1].num, 5);
+    });
   });
-  it('When no default max is provided, SuperFizzBuzz is initialized with a max of 100', () => {
-    assert.equal(fizzBuzz.max, 100);
-  });
-  it('When no default wordNums is provided, SuperFizzBuzz is initialized with wordNums of [{word: "Fizz", num: 3}, {word: "Buzz", num:5}]', () => {
-    assert.equal(fizzBuzz.wordNums[0].word, "Fizz");
-    assert.equal(fizzBuzz.wordNums[0].num, 3);
-    assert.equal(fizzBuzz.wordNums[1].word, "Buzz");
-    assert.equal(fizzBuzz.wordNums[1].num, 5);
-  });
+
+  
 });
